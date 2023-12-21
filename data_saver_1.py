4a68be9b68a81870235bf875cfe7be2ce513fbc1
satyamsaha <142487519+satyamsaha@users.noreply.github.com>
2023-12-21 16:37:52 +0530

    Add files via upload

--- data_saver_1.py
+++ data_saver_1.py
@@ -1,11 +1,17 @@
 import rospy
 import csv
+import numpy as np
 from geometry_msgs.msg import TwistStamped, PoseStamped
 from mavros_msgs.msg import State, RCOut
 from message_filters import ApproximateTimeSynchronizer, Subscriber
+import message_filters
+from tf.transformations import euler_from_quaternion
 
 # Initialize synchronized data lists
 synchronized_data = []
+d=0.18
+cT=0.00051566
+cM=1.3785541*(10**(-5))
 
 def synchronized_callback(velocity_msg, pose_msg, rcout_msg):
     timestamp = rospy.Time.now().to_sec()
@@ -13,19 +19,30 @@ def synchronized_callback(velocity_msg, pose_msg, rcout_msg):
     velocity_data = [velocity_msg.twist.linear.x, velocity_msg.twist.linear.y, velocity_msg.twist.linear.z]
     angular_velocity_data = [velocity_msg.twist.angular.x, velocity_msg.twist.angular.y, velocity_msg.twist.angular.z]
     pose_orientation = [pose_msg.pose.orientation.x, pose_msg.pose.orientation.y, pose_msg.pose.orientation.z, pose_msg.pose.orientation.w]
-    pose_data = [pose_msg.pose.position.x, pose_msg.pose.position.y, pose_msg.pose.position.z] + pose_orientation
-    pwm_values = list(rcout_msg.channels[:4])  # Convert tuple to list
-
-    synchronized_data.append([timestamp] + velocity_data + angular_velocity_data + pose_data + pwm_values)
-
+    pose_data = [pose_msg.pose.position.x, pose_msg.pose.position.y, pose_msg.pose.position.z]
+    pwm_values = list(rcout_msg.channels[:4]) 
+    thrust_values=[3.61588461*(10**(-4))*pwm_values[0]-0.378894041,3.61588461*(10**(-4))*pwm_values[1]-0.378894041,3.61588461*(10**(-4))*pwm_values[2]-0.378894041,3.61588461*(10**(-4))*pwm_values[3]-0.378894041]
+    total_thrust=((3.61588461*(10**(-4))*pwm_values[0]-0.378894041)+(3.61588461*(10**(-4))*pwm_values[1]-0.378894041)+(3.61588461*(10**(-4))*pwm_values[2]-0.378894041)+(3.61588461*(10**(-4))*pwm_values[3]-0.378894041))
+    rpm1=-0.0135731379*(pwm_values[0]**2)+42.0025634*pwm_values[0]-28306.0077
+    rpm2=-0.0135731379*(pwm_values[1]**2)+42.0025634*pwm_values[1]-28306.0077
+    rpm3=-0.0135731379*(pwm_values[2]**2)+42.0025634*pwm_values[2]-28306.0077
+    rpm4=-0.0135731379*(pwm_values[3]**2)+42.0025634*pwm_values[3]-28306.0077
+    rpm_values=[rpm1,rpm2,rpm3,rpm4]
+    moment_x= d*cT*(0.707*(rpm1**2)-0.707*(rpm2**2)-0.707*(rpm3**2)+0.707*(rpm2**2))*((2*np.pi/60)**2)
+    moment_y= d*cT*(0.707*(rpm1**2)+0.707*(rpm2**2)-0.707*(rpm3**2)-0.707*(rpm2**2))*((2*np.pi/60)**2)
+    moment_z= cM*((rpm1**2)-(rpm2**2)+(rpm3*2)-(rpm4**2))*((2*np.pi/60)**2)
+    moments=[moment_x,moment_y,moment_z]
+    (roll, pitch, yaw) = euler_from_quaternion (pose_orientation)
+    synchronized_data.append([timestamp] + velocity_data + angular_velocity_data + pose_data+[roll]+[pitch]+[yaw] + pwm_values+thrust_values+[total_thrust]+rpm_values+moments)
+    
 if __name__ == "__main__":
     rospy.init_node("data_recorder")
 
-    velocity_sub = Subscriber("/mavros/local_position/velocity_local", TwistStamped)
-    pose_sub = Subscriber("/mavros/local_position/pose", PoseStamped)
-    rcout_sub = Subscriber("/mavros/rc/out", RCOut)
+    velocity_sub = message_filters.Subscriber("/mavros/local_position/velocity_local", TwistStamped) #
+    pose_sub = message_filters.Subscriber("/mavros/local_position/pose", PoseStamped)
+    rcout_sub = message_filters.Subscriber("/mavros/rc/out", RCOut)
 
-    ts = ApproximateTimeSynchronizer([velocity_sub, pose_sub, rcout_sub], queue_size=10, slop=0.1)
+    ts = ApproximateTimeSynchronizer([velocity_sub, pose_sub, rcout_sub], queue_size=100, slop=0.1)
     ts.registerCallback(synchronized_callback)
 
     rate = rospy.Rate(100)  # Recording rate in Hz
@@ -58,15 +75,15 @@ if __name__ == "__main__":
 
         if recording_started:
             # Save synchronized data to CSV file
-            file_name = "synchronized_data.csv"
+            file_name = "synchronized_data3.csv"
             with open(file_name, "w") as csv_file:
                 writer = csv.writer(csv_file)
                 header_row = ["Time",
                               "Linear_Vel_X", "Linear_Vel_Y", "Linear_Vel_Z",
                               "Angular_Vel_X", "Angular_Vel_Y", "Angular_Vel_Z",
                               "Position_X", "Position_Y", "Position_Z",
-                              "Orientation_X", "Orientation_Y", "Orientation_Z", "Orientation_W",
-                              "PWM1", "PWM2", "PWM3", "PWM4"]
+                              "roll", "pitch", "yaw",
+                              "PWM1", "PWM2", "PWM3", "PWM4","Thrust1","Thrust2","Thrust3","Thrust4","total_thrust","rpm1","rpm2","rpm3","rpm4","moment_x","moment_y","moment_z"]
                 writer.writerow(header_row)
 
                 for row in synchronized_data:
